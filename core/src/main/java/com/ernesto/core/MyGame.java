2c035adcaee1a8c0d0050da6e8640e621f065d6a
Ernestas Mitkus <ernestas@inventi.lt>
2015-07-08 14:11:03 +0300

    Remove unnecessary resources

--- core/src/main/java/com/ernesto/core/MyGame.java
+++ core/src/main/java/com/ernesto/core/MyGame.java
@@ -1,17 +1,13 @@
 package com.ernesto.core;
 
 import com.badlogic.gdx.graphics.GL30;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 
 public class MyGame implements ApplicationListener {
-	private SpriteBatch batch;
 
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
 	}
 
 	@Override
@@ -22,9 +18,6 @@ public class MyGame implements ApplicationListener {
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-
-		batch.begin();
-		batch.end();
 	}
 
 	@Override
